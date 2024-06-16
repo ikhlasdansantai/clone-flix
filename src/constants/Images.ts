0a135b99e6e4d0d95737753175f4db0e4ecdfeb5
ikhlasdansantai <initesting5@gmail.com>
2024-06-16 13:23:34 +0700

    refactor: membungkus semua gambar static yang akan digunakan

---
+++ src/constants/Images.ts
@@ -0,0 +1,11 @@
+import Hero from "/img__hero.png";
+import Tv from "/img__tv.png";
+import Pb from "/img__movie1.png";
+import Sp from "/img__movie2.png";
+import Ac from "/img__movie3.png";
+import Jk from "/img__movie4.png";
+import GooglePlay from "/img__GooglePlay.png";
+import AppStore from "/img__AppStore.png";
+import Jk from "/img__movie4.png";
+
+export default { Hero, Tv, Pb, Sp, Ac, Jk, GooglePlay, AppStore };
