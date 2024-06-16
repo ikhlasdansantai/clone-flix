46d7bec32906f5f46d0b5db48ad983afa8c29b67
ikhlasdansantai <initesting5@gmail.com>
2024-06-16 13:45:38 +0700

    refactor: wrap all static assets

--- src/constants/Images.ts
+++ src/constants/Images.ts
@@ -6,6 +6,5 @@ import Ac from "/img__movie3.png";
 import Jk from "/img__movie4.png";
 import GooglePlay from "/img__GooglePlay.png";
 import AppStore from "/img__AppStore.png";
-import Jk from "/img__movie4.png";
 
 export default { Hero, Tv, Pb, Sp, Ac, Jk, GooglePlay, AppStore };
